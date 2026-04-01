30a393caa19b4c0d60a63361cc5a72ef424fe461
Shabbir <shabbirmetar88@gmail.com>
2026-04-01 19:06:35 +0530

    "01"

---
+++ 01-HelloWorld/hello.js
@@ -0,0 +1,3 @@
+console.log("hey there my name is shabbir and i am starting node js");
+// npm init : to create package.json(it is a configuration file)
+// npm is node package manager: which contains modules, libraries, and packages
